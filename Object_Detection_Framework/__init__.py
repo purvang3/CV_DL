de934320557e49940df0b576b5e584458c5ec97b
Purvang <purvang@everestlabs.ai>
2020-12-21 09:37:38 -0800

    .

--- Object_Detection_Framework/__init__.py
+++ Object_Detection_Framework/__init__.py
@@ -12,7 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 
-# Second Author
+# Added more functionality by
 
 
 Copyright 2020-2021 purvang lapsiwala
